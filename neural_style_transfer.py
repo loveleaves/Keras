9e9312ed847eaadf571c5db16c49a36bb536aa1b
tony <56626062+loveleaves@users.noreply.github.com>
2021-02-03 22:48:28 +0800

    Update neural_style_transfer.py

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -1,39 +1,3 @@
-"""
-Title: Neural style transfer
-Author: [fchollet](https://twitter.com/fchollet)
-Date created: 2016/01/11
-Last modified: 2020/05/02
-Description: Transfering the style of a reference image to target image using gradient descent.
-"""
-
-"""
-## Introduction
-Style transfer consists in generating an image
-with the same "content" as a base image, but with the
-"style" of a different picture (typically artistic).
-This is achieved through the optimization of a loss function
-that has 3 components: "style loss", "content loss",
-and "total variation loss":
-- The total variation loss imposes local spatial continuity between
-the pixels of the combination image, giving it visual coherence.
-- The style loss is where the deep learning keeps in --that one is defined
-using a deep convolutional neural network. Precisely, it consists in a sum of
-L2 distances between the Gram matrices of the representations of
-the base image and the style reference image, extracted from
-different layers of a convnet (trained on ImageNet). The general idea
-is to capture color/texture information at different spatial
-scales (fairly large scales --defined by the depth of the layer considered).
-- The content loss is a L2 distance between the features of the base
-image (extracted from a deep layer) and the features of the combination image,
-keeping the generated image close enough to the original one.
-**Reference:** [A Neural Algorithm of Artistic Style](
-  http://arxiv.org/abs/1508.06576)
-"""
-
-"""
-## Setup
-"""
-
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -43,34 +7,33 @@ base_image_path = keras.utils.get_file("paris.jpg", "https://i.imgur.com/F28w3Ac
 style_reference_image_path = keras.utils.get_file(
     "starry_night.jpg", "https://i.imgur.com/9ooB60I.jpg"
 )
+"""
+如果想训练自己的图片，上面代码改成如下代码：
+base_image_path=“your_path.jpg” # base_image_path为待风格迁移的图片地址
+style_reference_image_path="your_path.jpg" # style_reference_image_path 为风格样式图片地址
+"""
+
 result_prefix = "paris_generated"
 
-# Weights of the different loss components
+# 各部分损失的权重设置
 total_variation_weight = 1e-6
 style_weight = 1e-6
 content_weight = 2.5e-8
 
-# Dimensions of the generated picture.
+# 生成图片的尺寸
 width, height = keras.preprocessing.image.load_img(base_image_path).size
 img_nrows = 400
 img_ncols = int(width * img_nrows / height)
 
-"""
-## Let's take a look at our base (content) image and our style reference image
-"""
-
+#通过下面命令查看要进行风格迁移的基本图片和样式参考图片
 from IPython.display import Image, display
 
 display(Image(base_image_path))
 display(Image(style_reference_image_path))
 
-"""
-## Image preprocessing / deprocessing utilities
-"""
-
-
+# 图像预处理
 def preprocess_image(image_path):
-    # Util function to open, resize and format pictures into appropriate tensors
+    # 利用Keras库函数的来打开图片，调整图片大小并将其格式化为适当的张量
     img = keras.preprocessing.image.load_img(
         image_path, target_size=(img_nrows, img_ncols)
     )
@@ -81,9 +44,9 @@ def preprocess_image(image_path):
 
 
 def deprocess_image(x):
-    # Util function to convert a tensor into a valid image
+    # 再利用函数将张量转换为有效图像
     x = x.reshape((img_nrows, img_ncols, 3))
-    # Remove zero-center by mean pixel
+    # 通过平均像素去除零中心
     x[:, :, 0] += 103.939
     x[:, :, 1] += 116.779
     x[:, :, 2] += 123.68
@@ -93,20 +56,7 @@ def deprocess_image(x):
     return x
 
 
-"""
-## Compute the style transfer loss
-First, we need to define 4 utility functions:
-- `gram_matrix` (used to compute the style loss)
-- The `style_loss` function, which keeps the generated image close to the local textures
-of the style reference image
-- The `content_loss` function, which keeps the high-level representation of the
-generated image close to that of the base image
-- The `total_variation_loss` function, a regularization loss which keeps the generated
-image locally-coherent
-"""
-
-# The gram matrix of an image tensor (feature-wise outer product)
-
+# 图像张量的gram矩阵（特征矩阵和特征矩阵转置的乘积）
 
 def gram_matrix(x):
     x = tf.transpose(x, (2, 0, 1))
@@ -114,13 +64,9 @@ def gram_matrix(x):
     gram = tf.matmul(features, tf.transpose(features))
     return gram
 
-
-# The "style loss" is designed to maintain
-# the style of the reference image in the generated image.
-# It is based on the gram matrices (which capture style) of
-# feature maps from the style reference image
-# and from the generated image
-
+# “风格损失”旨在保持生成图像中参考图像的样式。
+# 它基于的gram矩阵（样式提取）来自样式参考图像
+# 和从它生成的图像的特征图
 
 def style_loss(style, combination):
     S = gram_matrix(style)
@@ -129,19 +75,14 @@ def style_loss(style, combination):
     size = img_nrows * img_ncols
     return tf.reduce_sum(tf.square(S - C)) / (4.0 * (channels ** 2) * (size ** 2))
 
-
-# An auxiliary loss function
-# designed to maintain the "content" of the
-# base image in the generated image
-
+# 辅助损失函数设计来是为了
+# 维护生成的图像中的基本图像的内容
 
 def content_loss(base, combination):
     return tf.reduce_sum(tf.square(combination - base))
 
-
-# The 3rd loss function, total variation loss,
-# designed to keep the generated image locally coherent
-
+# 第三个损失函数是总变化损失，
+# 设计此函数是为了使生成的图像保持局部连贯。
 
 def total_variation_loss(x):
     a = tf.square(
@@ -154,25 +95,23 @@ def total_variation_loss(x):
 
 
 """
-Next, let's create a feature extraction model that retrieves the intermediate activations
-of VGG19 (as a dict, by name).
+接下来，让我们创建一个特征提取模型，该模型检索VGG19的中间激活（根据名字制成字典）。
 """
 
-# Build a VGG19 model loaded with pre-trained ImageNet weights
+# 建立一个加载了已经训练好的ImageNet的权重的VGG19模型
 model = vgg19.VGG19(weights="imagenet", include_top=False)
 
-# Get the symbolic outputs of each "key" layer (we gave them unique names).
+# 获取每个“关键”层的符号输出（我们给它们指定了唯一的名称）。
 outputs_dict = dict([(layer.name, layer.output) for layer in model.layers])
 
-# Set up a model that returns the activation values for every layer in
-# VGG19 (as a dict).
+# 建立一个模型，以返回VGG19中每层的激活值（以字典的方式）。
 feature_extractor = keras.Model(inputs=model.inputs, outputs=outputs_dict)
 
 """
-Finally, here's the code that computes the style transfer loss.
+最后，这是计算样式转移损失的代码。
 """
 
-# List of layers to use for the style loss.
+# 用于样式丢失的图层列表。
 style_layer_names = [
     "block1_conv1",
     "block2_conv1",
@@ -180,7 +119,7 @@ style_layer_names = [
     "block4_conv1",
     "block5_conv1",
 ]
-# The layer to use for the content loss.
+# 用于内容丢失的层。
 content_layer_name = "block5_conv2"
 
 
@@ -190,17 +129,17 @@ def compute_loss(combination_image, base_image, style_reference_image):
     )
     features = feature_extractor(input_tensor)
 
-    # Initialize the loss
+    # 初始化损失
     loss = tf.zeros(shape=())
 
-    # Add content loss
+    # 加入内容丢失
     layer_features = features[content_layer_name]
     base_image_features = layer_features[0, :, :, :]
     combination_features = layer_features[2, :, :, :]
     loss = loss + content_weight * content_loss(
         base_image_features, combination_features
     )
-    # Add style loss
+    # 加入风格损失
     for layer_name in style_layer_names:
         layer_features = features[layer_name]
         style_reference_features = layer_features[1, :, :, :]
@@ -208,16 +147,15 @@ def compute_loss(combination_image, base_image, style_reference_image):
         sl = style_loss(style_reference_features, combination_features)
         loss += (style_weight / len(style_layer_names)) * sl
 
-    # Add total variation loss
+    # 加入总变化损失
     loss += total_variation_weight * total_variation_loss(combination_image)
     return loss
 
 
 """
-## Add a tf.function decorator to loss & gradient computation
-To compile it, and thus make it fast.
+将tf.function装饰器添加到损耗计算和梯度计算中
+使在编译过程中能运行更快
 """
-
 
 @tf.function
 def compute_loss_and_grads(combination_image, base_image, style_reference_image):
@@ -226,12 +164,9 @@ def compute_loss_and_grads(combination_image, base_image, style_reference_image)
     grads = tape.gradient(loss, combination_image)
     return loss, grads
 
-
 """
-## The training loop
-Repeatedly run vanilla gradient descent steps to minimize the loss, and save the
-resulting image every 100 iterations.
-We decay the learning rate by 0.96 every 100 steps.
+重复执行批量梯度下降步骤，以最大程度地减少损失，并每100次迭代保存生成的图像。
+每100步将学习率降低0.96。
 """
 
 optimizer = keras.optimizers.SGD(
@@ -257,7 +192,7 @@ for i in range(1, iterations + 1):
         keras.preprocessing.image.save_img(fname, img)
 
 """
-After 4000 iterations, you get the following result:
+经过4000次迭代，输出结果：
 """
 
 display(Image(result_prefix + "_at_iteration_4000.png"))
