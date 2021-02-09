1c08de9997abc4fd24667a96df0a18bfd44ddf36
tony <56626062+loveleaves@users.noreply.github.com>
2021-02-09 18:54:45 +0800

    Update deep_dream.py

--- deep_dream.py
+++ deep_dream.py
@@ -1,46 +1,44 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from IPython.display import Image, display
 from tensorflow.keras.applications import inception_v3
+"""
+报以下错误：
+File "D:\Anaconda3\envs\TF2.1\lib\site-packages\tensorflow_core\python\keras\saving\hdf5_format.py", line 651, in load_weights_from_hdf5_group
+    original_keras_version = f.attrs['keras_version'].decode('utf8')
+AttributeError: 'str' object has no attribute 'decode'
+解决方案：
+pip install h5py==2.10 -i https://pypi.tuna.tsinghua.edu.cn/simple/
+
+"""
 
 base_image_path = keras.utils.get_file("sky.jpg", "https://i.imgur.com/aGBdQyK.jpg")
 result_prefix = "sky_dream"
+"""
+如果训练其他图片，上面两行代码换成下面两行代码,其中picture.jpg换成你图片的名字
+base_image_path = "F28w3Ac.jpg"
+result_prefix = base_image_path.rstrip('.jpg') + "_dream"
+"""
 
-# These are the names of the layers
-# for which we try to maximize activation,
-# as well as their weight in the final loss
-# we try to maximize.
-# You can tweak these setting to obtain new visual effects.
+#这些是我们尝试最大化激活的层的名称，以及我们试图最大化激活的最终损失的权重。
+# 你可以调整这些设置以获得新的视觉效果。
 layer_settings = {
     "mixed4": 1.0,
-    "mixed5": 1.5,
+    "mixed5": 2.0,
     "mixed6": 2.0,
     "mixed7": 2.5,
 }
 
-# Playing with these hyperparameters will also allow you to achieve new effects
-step = 0.01  # Gradient ascent step size
-num_octave = 3  # Number of scales at which to run gradient ascent
-octave_scale = 1.4  # Size ratio between scales
-iterations = 20  # Number of ascent steps per scale
+# 利用这些超参数也可以获得新的视觉效果
+step = 0.01  # 梯度上升步长
+num_octave = 3
+octave_scale = 1.4
+iterations = 20
 max_loss = 15.0
 
-"""
-This is our base image:
-"""
-
-from IPython.display import Image, display
-
-display(Image(base_image_path))
-
-"""
-Let's set up some image preprocessing/deprocessing utilities:
-"""
-
-
 def preprocess_image(image_path):
-    # Util function to open, resize and format pictures
-    # into appropriate arrays.
+    # 函数功能：调整和格式化图片到正确大小的数组
     img = keras.preprocessing.image.load_img(image_path)
     img = keras.preprocessing.image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
@@ -49,27 +47,24 @@ def preprocess_image(image_path):
 
 
 def deprocess_image(x):
-    # Util function to convert a NumPy array into a valid image.
+    # 函数功能：把NumPy数组转成一张有效图片
     x = x.reshape((x.shape[1], x.shape[2], 3))
-    # Undo inception v3 preprocessing
+    # 撤消Inception v3预处理
     x /= 2.0
     x += 0.5
     x *= 255.0
-    # Convert to uint8 and clip to the valid range [0, 255]
+    # 转换为uint8并裁剪到有效范围[0，255]
     x = np.clip(x, 0, 255).astype("uint8")
     return x
 
-
 """
-## Compute the Deep Dream loss
-First, build a feature extraction model to retrieve the activations of our target layers
-given an input image.
+    计算模型损失
 """
 
-# Build an InceptionV3 model loaded with pre-trained ImageNet weights
+# 建立一个预训练好ImageNet的Inception V3模型
 model = inception_v3.InceptionV3(weights="imagenet", include_top=False)
 
-# Get the symbolic outputs of each "key" layer (we gave them unique names).
+# 获取每个“关键”层的符号输出（我们给它们指定了唯一的名称）。
 outputs_dict = dict(
     [
         (layer.name, layer.output)
@@ -77,31 +72,20 @@ outputs_dict = dict(
     ]
 )
 
-# Set up a model that returns the activation values for every target layer
-# (as a dict)
+# 设置一个模型，该模型返回每个目标层的激活值 (以字典方式)
 feature_extractor = keras.Model(inputs=model.inputs, outputs=outputs_dict)
-
-"""
-The actual loss computation is very simple:
-"""
-
 
 def compute_loss(input_image):
     features = feature_extractor(input_image)
-    # Initialize the loss
+    # 初始化损失
     loss = tf.zeros(shape=())
     for name in features.keys():
         coeff = layer_settings[name]
         activation = features[name]
-        # We avoid border artifacts by only involving non-border pixels in the loss.
+        # 通过仅在损失中涉及非边界像素，我们避免了边界伪影。
         scaling = tf.reduce_prod(tf.cast(tf.shape(activation), "float32"))
         loss += coeff * tf.reduce_sum(tf.square(activation[:, 2:-2, 2:-2, :])) / scaling
     return loss
-
-
-"""
-## Set up the gradient ascent loop for one octave
-"""
 
 
 @tf.function
@@ -109,9 +93,9 @@ def gradient_ascent_step(img, learning_rate):
     with tf.GradientTape() as tape:
         tape.watch(img)
         loss = compute_loss(img)
-    # Compute gradients.
+    # 计算梯度
     grads = tape.gradient(loss, img)
-    # Normalize gradients.
+    # 归一化梯度
     grads /= tf.maximum(tf.reduce_mean(tf.abs(grads)), 1e-6)
     img += learning_rate * grads
     return loss, img
@@ -126,10 +110,6 @@ def gradient_ascent_loop(img, iterations, learning_rate, max_loss=None):
     return img
 
 
-"""
-## Run the training loop, iterating over different octaves
-"""
-
 original_img = preprocess_image(base_image_path)
 original_shape = original_img.shape[1:3]
 
@@ -140,7 +120,7 @@ for i in range(1, num_octave):
 successive_shapes = successive_shapes[::-1]
 shrunk_original_img = tf.image.resize(original_img, successive_shapes[0])
 
-img = tf.identity(original_img)  # Make a copy
+img = tf.identity(original_img)  # 准备一个备份
 for i, shape in enumerate(successive_shapes):
     print("Processing octave %d with shape %s" % (i, shape))
     img = tf.image.resize(img, shape)
@@ -156,8 +136,5 @@ for i, shape in enumerate(successive_shapes):
 
 keras.preprocessing.image.save_img(result_prefix + ".png", deprocess_image(img.numpy()))
 
-"""
-Display the result.
-"""
 
-display(Image(result_prefix + ".png"))
+display(Image(result_prefix + ".png")) # 训练结果图
